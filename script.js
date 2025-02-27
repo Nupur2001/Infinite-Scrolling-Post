7b7cc7c9fad1d892ba820f31ca355922e3106450
Nupur Sikka <67430495+Nupur2001@users.noreply.github.com>
2025-02-27 13:16:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,22 @@ document.addEventListener("DOMContentLoaded", () => {
   // let url = "https://jsonplaceholder.typicode.com/posts";
 
   let currentPage = 1;
+  let numPost=5
+  let postNumber=1
   let isFetching = false;
   let hasMore = true;
+
+function firstLetterCapitalize(letter){
+return letter.charAt(0).toUpperCase()+letter.splice(0)
+}
 
   async function fetchData() {
     isFetching = true;
     let response = await fetch(
-      `https://jsonplaceholder.typicode.com/posts?_page=${currentPage}`
+      `https://jsonplaceholder.typicode.com/posts?_limit=${numPost}&_page=${currentPage}`
     );
 
-    let data = await response.json;
+    let data = await response.json();
     console.log(data);
     isFetching = false;
 
@@ -21,34 +27,45 @@ document.addEventListener("DOMContentLoaded", () => {
       hasMore = false;
       return;
     }
-    for (let post of data) {
+    for (let postData of data) {
       let post = document.createElement("div");
       post.classList.add("post");
 
       let circle = document.createElement("span");
       circle.classList.add("circle");
+      circle.textContent=postNumber
+      postNumber++
 
       let postInfo = document.createElement("div");
       postInfo.classList.add("postInfo");
 
       let postTitle = document.createElement("h2");
       postTitle.classList.add("postTitle");
-      postTitle = `${post.title}`;
-      
+      postTitle.textContent = `${postData.title}`;
+
       let postDescription = document.createElement("p");
       postDescription.classList.add("postDescription");
-      postDescription = `${post.body}`;
-
-      post.appendChild(postInfo);
-      postInfo.innerHTML = `
-        // ${postTitle}
-        // ${postDescription}
-
-        `;
+      postDescription.textContent = `${postData.body}`;
 
       postContainer.appendChild(post);
+      post.appendChild(postInfo);
+      post.appendChild(circle)
+      postInfo.appendChild(postTitle);
+      postInfo.appendChild(postDescription);
     }
+    currentPage++;
   }
   //   console.log(fetchData())
   fetchData();
+
+  window.addEventListener("scroll", () => {
+    if (isFetching || !hasMore) {
+      return;
+    }
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    //   console.log("Touched Bottom");
+    // fetchData();
+
+    }
+  });
 });
